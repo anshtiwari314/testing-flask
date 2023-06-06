e3dbdc6a6c7c0e6ab7fcc7447de9a9f8bf8476fc
anuj <anuj@localhost.localdomain>
2023-06-06 14:02:49 +0530

    code for py version

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
-
+import sys
 import os 
 load_dotenv()
 #print(f"0.0.0.0:{$PORT}")
 print(f"{os.getenv('PORT')}")
 saved_port = os.getenv('PORT')
 
+print(sys.version)
+
+
 app = Flask(__name__)
 
 @app.route("/", methods = ['POST'])
 def print_req():
-    
     user_as_json = request.get_json(silent=True, force=True)
     print(user_as_json)
     data = {'data': user_as_json}
