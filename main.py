c438d9e9715421ac5a854b37aff9dbb90f8368fc
anuj <anuj@localhost.localdomain>
2023-06-06 12:58:36 +0530

    add variable at top of file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 
+print(f"0.0.0.0:{PORT}")
+
 app = Flask(__name__)
 
 @app.route("/", methods = ['POST'])
 def print_req():
-    print(f"0.0.0.0:{PORT}")
+    
     user_as_json = request.get_json(silent=True, force=True)
     print(user_as_json)
     data = {'data': user_as_json}
