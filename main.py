7d22a6d318c16ff0db41ccdffc7fdc971235062a
anuj <anuj@localhost.localdomain>
2023-06-06 12:35:41 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['POST'])
+def print_req():
+    user_as_json = request.get_json(silent=True, force=True)
+    print(user_as_json)
+    data = {'data': user_as_json}
+    return jsonify(data, 200)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
